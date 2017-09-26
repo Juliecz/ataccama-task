2e753b1c61ccc6f66a45989aff25152a8197b40d
juliecz <yuliya@yuliya.cz>
2017-09-26 17:01:13 +0200

    Show/hide children tables

--- src/partials/Table/Table.js
+++ src/partials/Table/Table.js
@@ -2,45 +2,65 @@ import React, { Component } from 'react';
 import { Table as BTable, Glyphicon } from 'react-bootstrap';
 import './style.css';
 
-const Table = ({ content, name }) =>
-	(<BTable bordered className="p-table">
-		{ content.map((row, i) =>
-			(<tbody key={`tbody_${i}`}>
-			
-			{name && i===0 && <tr className="p-table-name">
-				<td colSpan={Object.keys(row.data).length+1}>{ name }</td>
-			</tr>}
-			
-			{ i===0 && <tr>
-				<th />
-				{ Object.keys(row.data).map((key, i) =>
-					(<th key={`th_${i}`}>{ key }</th>)) }
-			</tr> }
-			<tr>
-				{(row.kids && Object.keys(row.kids).length > 0)
-				? <td>
-					<Glyphicon glyph="triangle-bottom"/>
-				</td> : <td /> }
-				
-				{ Object.values(row.data).map((value, i) =>
-					(<td key={`td_${i}`}>{ value }</td>)) }
-			</tr>
-			
-			{(row.kids && Object.keys(row.kids).length > 0)
-			&& <tr>
-				<td colSpan={Object.keys(row.data).length+1}>
-					<Table
-						content={row.kids[Object.keys(row.kids)[0]].records}
-						name={Object.keys(row.kids)[0]}
-					/>
-				</td>
-			</tr>}
-			</tbody>))
+class Table extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			content: props.content.map((row) => ({ ...row, showKids: false })),
+			name: props.name
 		}
-	</BTable>);
+	}
+	
+	showKids = (i) => {
+		const arr = this.state.content;
+		arr[i].showKids = !arr[i].showKids;
+		this.setState({ content: [...arr] });
+	};
+	
+	render() {
+		const { content, name } = this.state;
+
+		return (<BTable bordered className="p-table">
+			{ content.map((row, i) =>
+				(<tbody key={`tbody_${i}`}>
+				
+				{name && i===0 && <tr className="p-table-name">
+					<td colSpan={Object.keys(row.data).length+1}>{ name }</td>
+				</tr>}
+				
+				{ i===0 && <tr>
+					<th />
+					{ Object.keys(row.data).map((key, i) =>
+						(<th key={`th_${i}`}>{ key }</th>)) }
+				</tr> }
+				<tr>
+					{(row.kids && Object.keys(row.kids).length > 0)
+						? <td onClick={() => this.showKids(i)}>
+							<Glyphicon glyph={ row.showKids ? 'triangle-bottom' : 'triangle-right'}/>
+						</td> : <td /> }
+					
+					{ Object.values(row.data).map((value, i) =>
+						(<td key={`td_${i}`}>{ value }</td>)) }
+				</tr>
+				
+				{(row.kids && row.showKids && Object.keys(row.kids).length > 0)
+				&& <tr>
+					<td colSpan={Object.keys(row.data).length+1}>
+						<Table
+							content={row.kids[Object.keys(row.kids)[0]].records}
+							name={Object.keys(row.kids)[0]}
+						/>
+					</td>
+				</tr>}
+				</tbody>))
+			}
+		</BTable>);
+	}
+}
 
 Table.defaultProps = {
-	content: []
+	content: [],
+	name: null
 };
 
 export default Table;
